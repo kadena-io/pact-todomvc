ac6f4674a6ac48c1dcae37aff33afee19b9a0bcc
buckie <wjmartino@gmail.com>
2017-02-13 09:56:35 -0500

    upgraded to new api
    Signed-off-by: buckie <wjmartino@gmail.com>

--- initialize-todos.sh
+++ initialize-todos.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [ -z "$1" ];
+then
+  echo 'Server port must be supplied; ex: ./initialize-todos.sh 9001'
+  exit 1
+fi
+
 # Public and private ed25519 keys
 pubKey='06c9c56daa8a068e1f19f5578cdf1797b047252e1ef0eb4a1809aa3c2226f61e'
 secKey='7ce4bae38fccfe33b6344b8c260bffa21df085cf033b3dc99b4781b550e1e922'
 
-./load.js -cf pact/todos.pact -n hello -s $secKey -p $pubKey -d '{"todo-admin-keyset":{"keys":["06c9c56daa8a068e1f19f5578cdf1797b047252e1ef0eb4a1809aa3c2226f61e"],"pred":"="}}' | curl -X POST -d @- http://localhost:$1/api/public/send
+./load.js -cf pact/todos.pact -n hello -s $secKey -p $pubKey -d '{"todo-admin-keyset":{"keys":["06c9c56daa8a068e1f19f5578cdf1797b047252e1ef0eb4a1809aa3c2226f61e"],"pred":"="}}' | curl -X POST -d @- http://localhost:$1/api/send
--- js/app.js
+++ js/app.js
@@ -2,181 +2,193 @@
 
 (function (exports) {
 
-	'use strict';
+  'use strict';
 
-	var filters = {
-		all: function (todos) {
-			return todos;
-		},
-		active: function (todos) {
-			return todos.filter(function (todo) {
-				return !todo.completed;
-			});
-		},
-		completed: function (todos) {
-			return todos.filter(function (todo) {
-				return todo.completed;
-			});
-		}
-	};
+  var filters = {
+    all: function (todos) {
+      return todos;
+    },
+    active: function (todos) {
+      return todos.filter(function (todo) {
+        return !todo.completed;
+      });
+    },
+    completed: function (todos) {
+      return todos.filter(function (todo) {
+        return todo.completed;
+      });
+    }
+  };
 
-	exports.app = new Vue({
+  exports.app = new Vue({
 
-		// the root element that will be compiled
-		el: '.todoapp',
+    // the root element that will be compiled
+    el: '.todoapp',
 
-		// app initial state
-		data: {
-			todos: [],
-			newTodo: '',
-			editedTodo: null,
-			visibility: 'all',
-      keyPair: Pact.crypto.genKeyPair()
-		},
-
-		// computed properties
-		// http://vuejs.org/guide/computed.html
-		computed: {
-			filteredTodos: function () {
-				return filters[this.visibility](this.todos);
-			},
-			remaining: function () {
-				return filters.active(this.todos).length;
-			},
-			allDone: {
-				get: function () {
-					return this.remaining === 0;
-				},
-				set: function (value) {
-					this.todos.forEach(function (todo) {
-						todo.completed = value;
-					});
-				}
-			}
-		},
-
-		// methods that implement data logic.
-		// note there's no DOM manipulation here at all.
-		methods: {
-
-        convertEntry: function(t) {
-            return {
-                "id": t.id,
-                "completed": (t.state === "completed"),
-                "title": t.entry};
-        },
-
-        pluralize: function (word, count) {
-            return word + (count === 1 ? '' : 's');
-        },
-
-        sendPactCmdSync: function(cmd, f) {
-            var msg = Pact.simple.exec.createCommand(this.keyPair, Date.now().toString(), cmd);
-            this.$http.post('/api/public/send', msg).then(function(resp) {
-                if (resp.body.status === "success") {
-                    var getResMsg = {"listen": resp.body.response.requestKeys[0]};
-                    this.$http.post('/api/listen', getResMsg).then(function(rkResp) {
-                        if (rkResp.body.status === "success") {
-                            if (f) {f(rkResp.body);}
-                        } else {
-                            console.log(rkResp.body);
-                        }
-                    });
-                } else {
-                    console.log(resp.body);
-                }
-            });
-        },
-
-        // we're keeping this todomvc simple and having the app update its todos fully every time
-        updateTodos: function(resp) {
-            var allTodos = resp.response.response.result.map(this.convertEntry).sort(function(a,b){
-                if (a.id < b.id) {
-                    return -1;
-                } else if (a.id > b.id) {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            });
-            this.todos = allTodos;
-        },
-
-        getAllTodos: function() {
-            var pactCmd = '(todos.read-todos)';
-            this.sendPactCmdSync(pactCmd, this.updateTodos);
-        },
-
-        addTodo: function () {
-            var value = this.newTodo && this.newTodo.trim();
-            if (!value) {
-                return;
-            }
-            this.newTodo = '';
-            //ex: (todos.new-todo 1)
-            var pactCmd = '(todos.new-todo ' + JSON.stringify(value) + ')';
-            this.sendPactCmdSync(pactCmd);
-            this.getAllTodos();
-        },
-
-        removeTodo: function (todo) {
-            var id = todo.id;
-            //ex: (todos.delete-todo 1)
-            var pactCmd = '(todos.delete-todo ' + JSON.stringify(id) + ')';
-            this.sendPactCmdSync(pactCmd);
-            this.getAllTodos();
-        },
-
-        editTodo: function (todo) {
-            this.beforeEditCache = todo.title;
-            this.editedTodo = todo;
-        },
-
-        doneEdit: function (todo) {
-            if (!this.editedTodo) {
-                return;
-            }
-            this.editedTodo = null;
-            todo.title = todo.title.trim();
-            if (!todo.title) {
-                this.removeTodo(todo);
-            } else {
-                var id = todo.id;
-                //ex: (todos.edit-todo 1 "foo")
-                var pactCmd = '(todos.edit-todo ' + JSON.stringify(id) + ' ' + JSON.stringify(todo.title) + ')';
-                this.sendPactCmdSync(pactCmd);
-                this.getAllTodos();
-            }
-        },
-
-        cancelEdit: function (todo) {
-            this.editedTodo = null;
-            todo.title = this.beforeEditCache;
-        },
-
-        removeCompleted: function () {
-            // for this, we're batching multiple (delete) commands into a single transaction
-            // ex: (todos.delete-todo 1)\n(todos.delete-todo 2)
-            var pactCmd = filters.completed(this.todos).map(function(todo){
-                    return '(todos.delete-todo ' + JSON.stringify(todo.id) + ')';
-                }).join("\n");
-            this.sendPactCmdSync(pactCmd);
-            this.getAllTodos();
-        }
+    // app initial state
+    data: {
+      todos: [],
+      newTodo: '',
+      editedTodo: null,
+      visibility: 'all',
+      keyPair: Pact.crypto.genKeyPair(),
+      enableDebug: false
     },
 
-        // a custom directive to wait for the DOM to be updated
-        // before focusing on the input field.
-        // http://vuejs.org/guide/custom-directive.html
-    directives: {
-        'todo-focus': function (el, binding) {
-            if (binding.value) {
-            el.focus();
-            }
+    // computed properties
+    // http://vuejs.org/guide/computed.html
+    computed: {
+      filteredTodos: function () {
+        return filters[this.visibility](this.todos);
+      },
+      remaining: function () {
+        return filters.active(this.todos).length;
+      },
+      allDone: {
+        get: function () {
+          return this.remaining === 0;
+        },
+        set: function (value) {
+          this.todos.forEach(function (todo) {
+            todo.completed = value;
+          });
         }
+      }
+    },
+
+    // methods that implement data logic.
+    // note there's no DOM manipulation here at all.
+    methods: {
+
+      convertEntry: function(t) {
+        return {
+          "id": t.id,
+          "completed": (t.state === "completed"),
+          "title": t.entry};
+        },
+
+      pluralize: function (word, count) {
+        return word + (count === 1 ? '' : 's');
+      },
+
+      debug: function(s) {
+        if (this.enableDebug) {
+          console.log(s);
+          return;
+        } else {
+          return;
+        }
+      },
+      sendPactCmdSync: function(cmd, f) {
+        var msg = Pact.simple.exec.createCommand(this.keyPair, Date.now().toString(), cmd);
+        this.debug(msg);
+        this.$http.post('/api/send', msg).then(function(resp) {
+          if (resp.body.status === "success") {
+            var getResMsg = {"listen": resp.body.response.requestKeys[0]};
+            this.debug(getResMsg);
+            this.$http.post('/api/listen', getResMsg).then(function(rkResp) {
+              if (rkResp.body.status === "success") {
+                this.debug(rkResp.body);
+                if (f) {f(rkResp.body);}
+              } else {
+                console.log(rkResp.body);
+              }
+            });
+          } else {
+            console.log(resp.body);
+          }
+        });
+      },
+
+      // we're keeping this todomvc simple and having the app update its todos fully every time
+      updateTodos: function(resp) {
+        var allTodos = resp.response.data.map(this.convertEntry).sort(function(a,b){
+          if (a.id < b.id) {
+            return -1;
+          } else if (a.id > b.id) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        this.todos = allTodos;
+      },
+
+      getAllTodos: function() {
+        var pactCmd = '(todos.read-todos)';
+        this.sendPactCmdSync(pactCmd, this.updateTodos);
+      },
+
+      addTodo: function () {
+        var value = this.newTodo && this.newTodo.trim();
+        if (!value) {
+          return;
+        }
+        this.newTodo = '';
+        //ex: (todos.new-todo 1)
+        var pactCmd = '(todos.new-todo ' + JSON.stringify(value) + ')';
+        this.sendPactCmdSync(pactCmd);
+        this.getAllTodos();
+      },
+
+      removeTodo: function (todo) {
+        var id = todo.id;
+        //ex: (todos.delete-todo 1)
+        var pactCmd = '(todos.delete-todo ' + JSON.stringify(id) + ')';
+        this.sendPactCmdSync(pactCmd);
+        this.getAllTodos();
+      },
+
+      editTodo: function (todo) {
+        this.beforeEditCache = todo.title;
+        this.editedTodo = todo;
+      },
+
+      doneEdit: function (todo) {
+        if (!this.editedTodo) {
+          return;
+        }
+        this.editedTodo = null;
+        todo.title = todo.title.trim();
+        if (!todo.title) {
+          this.removeTodo(todo);
+        } else {
+          var id = todo.id;
+          //ex: (todos.edit-todo 1 "foo")
+          var pactCmd = '(todos.edit-todo ' + JSON.stringify(id) + ' ' + JSON.stringify(todo.title) + ')';
+          this.sendPactCmdSync(pactCmd);
+          this.getAllTodos();
+        }
+      },
+
+      cancelEdit: function (todo) {
+        this.editedTodo = null;
+        todo.title = this.beforeEditCache;
+      },
+
+      removeCompleted: function () {
+        // for this, we're batching multiple (delete) commands into a single transaction
+        // ex: (todos.delete-todo 1)\n(todos.delete-todo 2)
+        var pactCmd = filters.completed(this.todos).map(function(todo){
+          return '(todos.delete-todo ' + JSON.stringify(todo.id) + ')';
+        }).join("\n");
+        this.sendPactCmdSync(pactCmd);
+        this.getAllTodos();
+      }
+    },
+
+    // a custom directive to wait for the DOM to be updated
+    // before focusing on the input field.
+    // http://vuejs.org/guide/custom-directive.html
+    directives: {
+      'todo-focus': function (el, binding) {
+        if (binding.value) {
+          el.focus();
+        }
+      }
     },
     mounted: function () {
-        this.getAllTodos();
+      this.getAllTodos();
     }
 });
 
